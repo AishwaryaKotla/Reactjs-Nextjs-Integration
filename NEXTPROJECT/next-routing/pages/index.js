fb0ff63886e07b01d368681ad5428c61c6edc71c
AishwaryaKotla <47515244+AishwaryaKotla@users.noreply.github.com>
2022-11-30 00:33:22 -0600

    programmatical navigation

--- NEXTPROJECT/next-routing/pages/index.js
+++ NEXTPROJECT/next-routing/pages/index.js
@@ -1,6 +1,13 @@
 import Link from 'next/link';
+import { useRouter} from 'next/router'
 
 function Home() {
+
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.replace('/product');
+    }
     return <div>
         <Link href='/blog'>
          
@@ -12,6 +19,9 @@ function Home() {
         <Link href='/product'>
             Click to navigate to the product page
         </Link>
+        <button onClick={handleClick}>
+            Place Order
+        </button>
     </div>
 }
 
